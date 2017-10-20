b5770032181d3421250f7cf60f9359fbbe8ac1a2
febinstephen <godsonfebin@gmail.com>
2017-10-20 12:57:37 +0530

    Added method index sample devices using DocType

--- utils/index_sample_data.py
+++ utils/index_sample_data.py
@@ -1,8 +1,13 @@
 from django.contrib.auth import get_user_model
 
+from datetime import datetime
+
 from applications.accounts.models import Device
+from applications.accounts.elastic.device import DeviceDocument
 
 
+
+# create and save devices using django models
 def index_devices():
     User = get_user_model()
     user = User.objects.all()[0]
@@ -11,4 +16,24 @@ def index_devices():
         device.user = user
         device.device_id = 'store_' + str(i)
         device.save()
+
+
+
+# create and save and device using DocType
+def add_devices_to_index():
+    # create and save a device
+    device = DeviceDocument(meta={'id': 40}, decvice_id='kingsmen')
+    now = datetime.now()
+
+    device.user = {
+        'mobile': '9447606310',
+        'email': 'febin@becoapp.in',
+    }
+
+    device.stores = {
+        'store_id': 'mgroad_2334',
+        'date': now,
+    }
+
+    device.save()
 
